cc324125c1bde67fc1f4119da84968d775c690b5
Mario-Chavez <marioachavez1@gmail.com>
2023-07-06 02:46:07 -0300

    se realizaron cambios de titulos

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,22 @@
-import "./App.css"
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container } from 'react-bootstrap'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
 import Formulario from "./components/Formulario";
 
-
 function App() {
-  return (
-    <>
-    <Container className="my-5 mainPage">
-       <h1 className="display-4 text-center">Pratico con ReactJs ejercicio 6</h1>
-       <hr className="text-light"/>
-      
-       <Formulario>
-        
-       </Formulario>
-    </Container>
-    <footer className="bg-dark text-light text-center py-5">
-      <p> &copy; Todos los derechos reservados</p>
-    </footer>
-    </>
-  )
+    return (
+        <>
+            <Container className="my-5 mainPage">
+                <h1 className="display-4 text-center">Lista de colores</h1>
+                <hr className="text-light" />
+
+                <Formulario></Formulario>
+            </Container>
+            <footer className="bg-dark text-light text-center py-5">
+                <p> &copy; Todos los derechos reservados</p>
+            </footer>
+        </>
+    );
 }
 
-export default App
+export default App;
